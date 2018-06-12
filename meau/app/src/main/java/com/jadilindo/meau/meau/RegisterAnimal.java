eafbb76eb619f7399b15d2b99279db00bb38f1e4
pedrobpio <pedrobpio@gmail.com>
2018-06-12 19:54:11 -0300

    layout cadastrar quase completo, faltam detalhes

--- meau/app/src/main/java/com/jadilindo/meau/meau/RegisterAnimal.java
+++ meau/app/src/main/java/com/jadilindo/meau/meau/RegisterAnimal.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import fragments.CadastrarAdocaoFragment;
 import fragments.CadastrarAdotarAjudarFragment;
@@ -15,22 +16,36 @@ import fragments.CadastrarApadrinharFragment;
 
 public class RegisterAnimal extends AppCompatActivity {
 
+    boolean flagb1=true, flagb2=false, flagb3=false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_animal);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        Button b1, b2, b3, buttonCadastro;
 
-
-    }
-
-    boolean flagb1=false, flagb2=false, flagb3=false;
-    public void startRegisterAnimalForm(View view){
-        Button b1, b2, b3;
         b1 = (Button) findViewById(R.id.button_adocao);
         b2 = (Button) findViewById(R.id.button_apadrinhar);
         b3 = (Button) findViewById(R.id.button_ajudar);
+        FragmentManager fm = getFragmentManager();
+
+        b1.setBackgroundColor(getResources().getColor(R.color.BackColorYellowButton));
+        b2.setBackgroundColor(getResources().getColor(R.color.UnselectButton));
+        b3.setBackgroundColor(getResources().getColor(R.color.UnselectButton));
+        fm.beginTransaction().replace(R.id.cadastrar_fragment, new CadastrarAdocaoFragment()).commit();
+        buttonCadastro = (Button) findViewById(R.id.button_cadastrar_animal);
+        buttonCadastro.setText("COLOCAR PARA ADOÇÃO");
+
+    }
+
+
+    public void startRegisterAnimalForm(View view){
+        Button b1, b2, b3, buttonCadastro;
+        b1 = (Button) findViewById(R.id.button_adocao);
+        b2 = (Button) findViewById(R.id.button_apadrinhar);
+        b3 = (Button) findViewById(R.id.button_ajudar);
+        buttonCadastro = (Button) findViewById(R.id.button_cadastrar_animal);
         FragmentManager fm = getFragmentManager();
         if (view == b1){
             flagb1 = !flagb1; //swich falg value
@@ -45,40 +60,56 @@ public class RegisterAnimal extends AppCompatActivity {
         }
 
         if(flagb1 && flagb3){
+            TextView T = (TextView) findViewById(R.id.cadastro_titulo);
+            T.setText("ADOÇÃO");
             b1.setBackgroundColor(getResources().getColor(R.color.BackColorYellowButton));
             b2.setBackgroundColor(getResources().getColor(R.color.UnselectButton));
             b3.setBackgroundColor(getResources().getColor(R.color.BackColorYellowButton));
             fm.beginTransaction().replace(R.id.cadastrar_fragment, new CadastrarAdotarAjudarFragment()).commit();
+            buttonCadastro.setText("COLOCAR PARA ADOÇÃO");
         }
         else if (flagb2 && flagb3){
+            TextView T = (TextView) findViewById(R.id.cadastro_titulo);
+            T.setText("APADRINHAR");
             b1.setBackgroundColor(getResources().getColor(R.color.UnselectButton));
             b2.setBackgroundColor(getResources().getColor(R.color.BackColorYellowButton));
             b3.setBackgroundColor(getResources().getColor(R.color.BackColorYellowButton));
             fm.beginTransaction().replace(R.id.cadastrar_fragment, new CadastrarApadrinharAjudarFragment()).commit();
+            buttonCadastro.setText("PROCURAR PADRINHO");
 
         }
         else if (flagb1){
+            TextView T = (TextView) findViewById(R.id.cadastro_titulo);
+            T.setText("ADOÇÃO");
             b1.setBackgroundColor(getResources().getColor(R.color.BackColorYellowButton));
             b2.setBackgroundColor(getResources().getColor(R.color.UnselectButton));
             b3.setBackgroundColor(getResources().getColor(R.color.UnselectButton));
             fm.beginTransaction().replace(R.id.cadastrar_fragment, new CadastrarAdocaoFragment()).commit();
+            buttonCadastro.setText("COLOCAR PARA ADOÇÃO");
         }
         else if (flagb2){
+            TextView T = (TextView) findViewById(R.id.cadastro_titulo);
+            T.setText("APADRINHAR");
             b1.setBackgroundColor(getResources().getColor(R.color.UnselectButton));
             b2.setBackgroundColor(getResources().getColor(R.color.BackColorYellowButton));
             b3.setBackgroundColor(getResources().getColor(R.color.UnselectButton));
             fm.beginTransaction().replace(R.id.cadastrar_fragment, new CadastrarApadrinharFragment()).commit();
+            buttonCadastro.setText("PROCURAR PADRINHO");
         }
         else if (flagb3){
+            TextView T = (TextView) findViewById(R.id.cadastro_titulo);
+            T.setText("AJUDAR");
             b1.setBackgroundColor(getResources().getColor(R.color.UnselectButton));
             b2.setBackgroundColor(getResources().getColor(R.color.UnselectButton));
             b3.setBackgroundColor(getResources().getColor(R.color.BackColorYellowButton));
             fm.beginTransaction().replace(R.id.cadastrar_fragment, new CadastrarAjudarFragment()).commit();
+            buttonCadastro.setText("PROCURAR AJUDA");
+
         }
         else {
-            b1.setBackgroundColor(getResources().getColor(R.color.UnselectButton));
-            b2.setBackgroundColor(getResources().getColor(R.color.UnselectButton));
-            b3.setBackgroundColor(getResources().getColor(R.color.UnselectButton));
+//            b1.setBackgroundColor(getResources().getColor(R.color.UnselectButton));
+//            b2.setBackgroundColor(getResources().getColor(R.color.UnselectButton));
+//            b3.setBackgroundColor(getResources().getColor(R.color.UnselectButton));
         }
 
 

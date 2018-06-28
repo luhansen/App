d1bf50500ad89e515a7052920d07cc55c957d1aa
Luiza <luizaahansen@gmail.com>
2018-06-27 21:32:47 -0300

    favoritos sem recicleview

--- meau/app/src/main/java/fragments/FavoritosFragment.java
+++ meau/app/src/main/java/fragments/FavoritosFragment.java
@@ -23,8 +23,6 @@ import com.jadilindo.meau.meau.R;
 import com.jadilindo.meau.meau.User;
 import com.squareup.picasso.Picasso;
 
-import java.util.ArrayList;
-
 /**
  * Created by pedro on 09/05/18.
  */
@@ -37,7 +35,7 @@ public class FavoritosFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_favoritos, container, false);
+        final View rootView = inflater.inflate(R.layout.fragment_favoritos, container, false);
         FirebaseAuth mAuth = FirebaseAuth.getInstance();
         FirebaseUser currentUser = mAuth.getCurrentUser();
         container_aux = rootView.findViewById(R.id.container_layout);
@@ -57,26 +55,45 @@ public class FavoritosFragment extends Fragment {
                                 container_aux.addView(warning);
                             }else {
                                 for (Animal animal : user.getFavorites()) {
+                                    ImageView imageElement = (ImageView) rootView.findViewById(R.id.image_pet);
                                     if (animal == null) continue;
-                                    ImageView imageViewAnimal = new ImageView(getActivity());
+//                                    ImageView imageViewAnimal = new ImageView(getActivity());
                                     Picasso.with(getActivity())
                                             .load(animal.getPicture())
-                                            .noFade().into(imageViewAnimal);
+                                            .noFade().into(imageElement);
 
-                                    TextView name_text_view = new TextView(getActivity());
-                                    name_text_view.setPadding(0, 40, 0, 0);
-                                    name_text_view.setText("Nome do animal: " + animal.getName());
-                                    TextView age_text_view = new TextView(getActivity());
-                                    age_text_view.setText("Idade do animal: " + animal.getAge());
-                                    TextView gender_text_view = new TextView(getActivity());
-                                    gender_text_view.setText("Sexo do animal: " + animal.getGender());
-                                    TextView size_text_view = new TextView(getActivity());
-                                    size_text_view.setText("Tamanho do animal: " + animal.getSize());
-                                    container_aux.addView(imageViewAnimal);
-                                    container_aux.addView(name_text_view);
-                                    container_aux.addView(age_text_view);
-                                    container_aux.addView(gender_text_view);
-                                    container_aux.addView(size_text_view);
+//                                    Bitmap myBitmap = BitmapFactory.decodeFile(animal.getPicture());
+//                                    File imgFile = new File(animal.getPicture());
+//                                    imageElement.set
+//                                    imageElement.setImageURI(Uri.fromFile(imgFile));
+
+//                                    Picasso.with(getActivity()).loa
+
+                                    TextView textElement = (TextView) rootView.findViewById(R.id.name_pet);
+                                    textElement.setText(animal.getName());
+                                    textElement = (TextView) rootView.findViewById(R.id.idade_pet);
+                                    textElement.setText(animal.getAge());
+                                    textElement = (TextView) rootView.findViewById(R.id.sexo_pet);
+                                    textElement.setText(animal.getGender());
+                                    textElement = (TextView) rootView.findViewById(R.id.tamanho_pet);
+                                    textElement.setText(animal.getSize());
+                                    textElement = (TextView) rootView.findViewById(R.id.localizacao_pet);
+                                    textElement.setText("vazio");
+
+//                                    TextView name_text_view = new TextView(getActivity());
+//                                    name_text_view.setPadding(0, 40, 0, 0);
+//                                    name_text_view.setText("Nome do animal: " + animal.getName());
+//                                    TextView age_text_view = new TextView(getActivity());
+//                                    age_text_view.setText("Idade do animal: " + animal.getAge());
+//                                    TextView gender_text_view = new TextView(getActivity());
+//                                    gender_text_view.setText("Sexo do animal: " + animal.getGender());
+//                                    TextView size_text_view = new TextView(getActivity());
+//                                    size_text_view.setText("Tamanho do animal: " + animal.getSize());
+//                                    container_aux.addView(imageViewAnimal);
+//                                    container_aux.addView(name_text_view);
+//                                    container_aux.addView(age_text_view);
+//                                    container_aux.addView(gender_text_view);
+//                                    container_aux.addView(size_text_view);
                                 }
                             }
                         }

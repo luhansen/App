6e0a69cfd5df6004d794a63e928451f58fbbc7f1
Luiza <luizaahansen@gmail.com>
2018-06-28 21:37:34 -0300

    recycler view da pagina meus pets

--- meau/app/src/main/java/fragments/PetsFragment.java
+++ meau/app/src/main/java/fragments/PetsFragment.java
@@ -1,16 +1,15 @@
 package fragments;
 
 import android.app.Fragment;
-import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -21,11 +20,9 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 import com.jadilindo.meau.meau.Animal;
-import com.jadilindo.meau.meau.MainActivity;
+import com.jadilindo.meau.meau.ListAdapter;
 import com.jadilindo.meau.meau.R;
-import com.jadilindo.meau.meau.RegisterAnimal;
 import com.jadilindo.meau.meau.User;
-import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
@@ -43,6 +40,15 @@ public class PetsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.fragment_pets, container, false);
+
+        RecyclerView recyclerView = rootView.findViewById(R.id.listRecycleViewMeusPets);
+        final ArrayList<Animal> animais = new ArrayList<>();
+        final ListAdapter listAdapter = new ListAdapter(animais);
+        recyclerView.setAdapter(listAdapter);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+
+
         FirebaseAuth mAuth = FirebaseAuth.getInstance();
         FirebaseUser currentUser = mAuth.getCurrentUser();
         container_aux = rootView.findViewById(R.id.container_layout);
@@ -61,27 +67,31 @@ public class PetsFragment extends Fragment {
                                 warning.setText("Nenhum animal");
                                 container_aux.addView(warning);
                             }else {
-                                for (Animal animal : user.getOwns()) {
-                                    if (animal == null) continue;
-                                    ImageView imageViewAnimal = new ImageView(getActivity());
-                                    Picasso.with(getActivity())
-                                            .load(animal.getPicture())
-                                            .noFade().into(imageViewAnimal);
-                                    TextView name_text_view = new TextView(getActivity());
-                                    name_text_view.setPadding(0, 40, 0, 0);
-                                    name_text_view.setText("Nome do animal: " + animal.getName());
-                                    TextView age_text_view = new TextView(getActivity());
-                                    age_text_view.setText("Idade do animal: " + animal.getAge());
-                                    TextView gender_text_view = new TextView(getActivity());
-                                    gender_text_view.setText("Sexo do animal: " + animal.getGender());
-                                    TextView size_text_view = new TextView(getActivity());
-                                    size_text_view.setText("Tamanho do animal: " + animal.getSize());
-                                    container_aux.addView(imageViewAnimal);
-                                    container_aux.addView(name_text_view);
-                                    container_aux.addView(age_text_view);
-                                    container_aux.addView(gender_text_view);
-                                    container_aux.addView(size_text_view);
-                                }
+                                animais.clear();
+                                animais.addAll(user.getOwns());
+
+                                listAdapter.notifyDataSetChanged();
+//                                for (Animal animal : user.getOwns()) {
+//                                    if (animal == null) continue;
+//                                    ImageView imageViewAnimal = new ImageView(getActivity());
+//                                    Picasso.with(getActivity())
+//                                            .load(animal.getPicture())
+//                                            .noFade().into(imageViewAnimal);
+//                                    TextView name_text_view = new TextView(getActivity());
+//                                    name_text_view.setPadding(0, 40, 0, 0);
+//                                    name_text_view.setText("Nome do animal: " + animal.getName());
+//                                    TextView age_text_view = new TextView(getActivity());
+//                                    age_text_view.setText("Idade do animal: " + animal.getAge());
+//                                    TextView gender_text_view = new TextView(getActivity());
+//                                    gender_text_view.setText("Sexo do animal: " + animal.getGender());
+//                                    TextView size_text_view = new TextView(getActivity());
+//                                    size_text_view.setText("Tamanho do animal: " + animal.getSize());
+//                                    container_aux.addView(imageViewAnimal);
+//                                    container_aux.addView(name_text_view);
+//                                    container_aux.addView(age_text_view);
+//                                    container_aux.addView(gender_text_view);
+//                                    container_aux.addView(size_text_view);
+//                                }
                             }
                         }
                     }
